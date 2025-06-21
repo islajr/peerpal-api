b435a069cfea2038771bebc5942e41185b9cc7f0
Isla <islarcherjr@icloud.com>
2025-06-21 17:14:11 +0100

    initial commit

---
+++ src/main/java/org/project/peerpalapi/PeerpalApiApplication.java
@@ -0,0 +1,13 @@
+package org.project.peerpalapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeerpalApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PeerpalApiApplication.class, args);
+    }
+
+}
