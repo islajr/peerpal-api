91cd718458f49458eb9f3102e3b877889fc88a20
Isla <islarcherjr@icloud.com>
2025-08-05 21:21:18 +0100

    update: cors update.

--- src/main/java/org/project/peerpalapi/security/CustomCorsConfiguration.java
+++ src/main/java/org/project/peerpalapi/security/CustomCorsConfiguration.java
@@ -5,16 +5,23 @@ import org.springframework.stereotype.Component;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 
+import java.util.Collections;
 import java.util.List;
 
 @Component
 public class CustomCorsConfiguration implements CorsConfigurationSource {
     @Override
     public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
+        String origin = request.getHeader("Origin");
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("*"));
-        config.setAllowedMethods(List.of("*"));
-        config.setAllowedHeaders(List.of("*"));
+
+        if (origin != null) {
+
+            config.setAllowedOrigins(Collections.singletonList(origin));
+        }
+        config.setAllowedMethods(Collections.singletonList("*"));
+        config.setAllowedHeaders(Collections.singletonList("*"));
+        config.setAllowCredentials(true);
 
         return config;
     }
